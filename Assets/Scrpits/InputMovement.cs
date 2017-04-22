6126234a0f9e2b8ca105e3f0d3adec40266124dd
Jipay <jipay.detiege@gmail.com>
2017-04-22 10:49:49 +0200

    Add the BEAUTIFUL planet

---
+++ Assets/Scrpits/InputMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class InputMovement : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D _rigidBody;
+
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float horizontal = 0;
+
+        horizontal = (int)Input.GetAxisRaw("Horizontal");
+
+        if(horizontal < 0)
+            _rigidBody.rotation += -Speed * Time.deltaTime;
+
+        else if(horizontal > 0)
+            _rigidBody.rotation += Speed * Time.deltaTime;
+
+    }
+}
