efaef65831522be3d5e4f17142b2791a836d4a1c
Jipay <jipay.detiege@gmail.com>
2017-04-23 12:13:38 +0200

    Fix bucket

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class InputManager : MonoSingleton<InputManager>
+{
+    public bool Action
+    {
+        get
+        {
+            bool value = _action;
+            _action = false;
+
+            return value;
+        }
+    }
+
+    private bool _action;
+
+    void FixedUpdate()
+    {
+        if(!_action)
+            _action = Input.GetButtonDown("Action");
+    }
+}
