4dfecee004a069bef3bcc9c37736d5d3427bb993
Kraghan <madjojo70@live.fr>
2017-04-23 12:14:06 +0200

    Add Rain

---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour {
+
+    public int FillLevel = 0;
+    public int CompleteLevel = 100;
+
+    public void Fill(int quantity)
+    {
+        FillLevel += quantity;
+        if(FillLevel >= CompleteLevel)
+        {
+            FillLevel = CompleteLevel;
+        }
+    }
+
+    public bool IsFull()
+    {
+        return FillLevel == CompleteLevel;
+    }
+
+	// Use this for initialization
+	void Start () {
+        FillLevel = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Rain.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rain : MonoBehaviour {
+
+    public int FillBucketQuantity = 1;
+    public float TimeUntilNextFill = 0.5f;
+    float timeElapsed = 0.0f;
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.gameObject.CompareTag("Weed"))
+        {
+
+        } 
+    }
+
+    void OnTriggerStay2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Seau"))
+        {
+            timeElapsed += Time.fixedDeltaTime;
+            if (timeElapsed >= TimeUntilNextFill)
+            {
+                Bucket bucket = coll.gameObject.GetComponent<Bucket>();
+                bucket.Fill(FillBucketQuantity);
+                timeElapsed = 0.0f;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Seau"))
+        {
+            timeElapsed = 0.0f;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        timeElapsed = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
