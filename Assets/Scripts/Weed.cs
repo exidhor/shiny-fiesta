b9125e5429722e7c711006cc446d4f5e1fed712e
Kraghan <madjojo70@live.fr>
2017-04-23 15:58:55 +0200

    Add ciel

--- Assets/Scripts/Weed.cs
+++ Assets/Scripts/Weed.cs
@@ -7,8 +7,32 @@ using UnityEngine;
 
 public class Weed : MonoBehaviour
 {
+    public int CurrentWater;
+    public int MaxWater = 300;
+    public Sprite[] sprites = new Sprite[3];
+
     public void ReceiveWater(int waterAmount)
     {
-        // todo
+        CurrentWater += waterAmount;
+        if (CurrentWater >= MaxWater)
+            CurrentWater = MaxWater;
     }
+
+    void Update()
+    {
+        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        if (CurrentWater < 100)
+            renderer.sprite = sprites[0];
+        else if (CurrentWater < 200)
+            renderer.sprite = sprites[1];
+        else
+            renderer.sprite = sprites[2];
+
+    }
+
+    void Start()
+    {
+        CurrentWater = 0;
+    }
+
 }
